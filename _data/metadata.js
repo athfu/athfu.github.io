e928e0e1a95c60a85bc92e66f55ed0532e7728b6
Athena Fung <athenafung25@gmail.com>
2023-05-19 22:59:26 +0100

    Initial commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "athena fung",
+	url: "https://example.com/",
+	language: "en",
+	description: "this is my blog",
+	author: {
+		name: "athenafung",
+		email: "athenafung25@gmail.com",
+		url: "https://example.com/about-me/",
+	},
+};
---
+++ content/about/index.md
@@ -0,0 +1,14 @@
+---
+layout: layouts/base.njk
+eleventyNavigation:
+  key: about
+  order: 3
+---
+
+# about
+
+hello!
+
+welcome to the thoughts and musings of a human in London.
+
+I am currently doing a batch at the [Recurse Center](https://www.recurse.com/).
